69ac6a68a331595ae7a7d9b31d0c719b1b130766
Marissa Langham <106556839+MaRissaLangham@users.noreply.github.com>
2024-03-17 00:33:47 -0500

    Game files

---
+++ brain/skills/Games/snaps.py
@@ -0,0 +1,5 @@
+""" This file Desiciption can be found in the comment below """
+# Author: Marissa Langham
+# Date: 03/15/2023
+# Description: Snaps game file for Ellie
+# File Name: snaps.py
---
+++ brain/skills/Games/twentyQuestions.py
@@ -0,0 +1,5 @@
+""" This file Desiciption can be found in the comment below """
+# Author: Marissa Langham
+# Date: 03/15/2023
+# Description: 20 QUestions game file for Ellie
+# File Name: twentyQuestions.py
