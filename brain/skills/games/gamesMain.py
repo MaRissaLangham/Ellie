7d9ec7cb8c2af043ef49723b551f3ff16cfedf5f
Marissa Langham <106556839+MaRissaLangham@users.noreply.github.com>
2024-03-24 23:50:13 -0500

    spelling

--- brain/skills/games/gamesMain.py
+++ brain/skills/games/gamesMain.py
@@ -21,6 +21,6 @@ def displayGameList():
 
 def whichGame(item):
     if gameList == item:
-           return f"Sure lets play {item} "
+           return f"Sure let's play {item} "
     else:
         return f"Oh I am sorry. I do not have the game {item}. please try again."
