627f309c7989685d8d23311eae00a60daebf17c9
Marissa Langham <106556839+MaRissaLangham@users.noreply.github.com>
2024-03-26 23:57:20 -0500

    Update guessNum.py

--- brain/skills/Games/guessNum.py
+++ brain/skills/Games/guessNum.py
@@ -4,3 +4,32 @@
 # Description: Guess the Number game file for Ellie
 # File Name: guessNum.py
 
+import random
+import speech_recognition as sr
+from gtts import gTTS
+import os
+
+"""def guessNumDifficulty(difficultyLevel):
+    
+
+
+def guessTheNumber(difficultyLevel):
+
+    difficulty_ranges = {1: 100, 2: 500, 3: 1000, 4: 10000, 5: 100000}
+    max_number = difficulty_ranges.get(difficultyLevel, 100)
+
+    if difficultyLevel == 1:
+    
+    elif difficultyLevel == 2:
+
+    elif difficultyLevel == 3:
+
+    elif difficultyLevel == 4:
+
+    elif difficultyLevel == 5:
+
+else:
+        guessNumDifficulty()
+    
+        number = random.randint(1, max_number) attempts = 0"""
+  
