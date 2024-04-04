6293ecdef4d76fb6378484d41d078513d342a14d
Marissa Langham <106556839+MaRissaLangham@users.noreply.github.com>
2024-04-04 17:59:13 -0500

    Update joke.py

--- brain/skills/jokes.py
+++ brain/skills/jokes.py
@@ -9,9 +9,10 @@ import pyjokes
 
 jokesList = [
     "Why don't scientists trust atoms? Because they make up everything.",
-    "Why did the scarecrow win an award? Because he was outstanding in his field."
-    "I told my computer I needed a break, and it said... No problem, I'll just go on a little byte."
-    
+    "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    "I told my computer I needed a break, and it said... No problem, I'll just go on a little byte.",
+    "Parallel lines have so much in common. It's a shame they'll never meet.",
+    "Did you hear about the mathematician who's afraid of negative numbers? He'll stop at nothing to avoid them."
 
 
 ]
